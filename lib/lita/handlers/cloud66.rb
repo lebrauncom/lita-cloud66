5fada848eb73261ab164c286029eb811f3ce78c4
lebrauncom <lebraun.com@gmail.com>
2016-04-15 14:45:40 +1000

    add simulated error handling

--- lib/lita/handlers/cloud66.rb
+++ lib/lita/handlers/cloud66.rb
@@ -1,14 +1,26 @@
 module Lita
   module Handlers
     class Cloud66 < Handler
+      BASE_URL = 'https://hooks.cloud66.com/stacks/redeploy'
+
       route(/deploy production|redeploy production/i, :deploy, command: false, help: {
         'deploy production' => 'Deploys the production server.',
         'redeploy production' => 'Redeploys the production server.'
       })
 
       def deploy(response)
-        response.reply "Starting redeployment of production server"
+        if deploy_server
+          response.reply "Starting redeployment of production server"
+        else
+          response.reply "Error deploying the production server"
+        end
       end
+
+      private
+
+        def deploy_server(server)
+          true
+        end
 
       Lita.register_handler(self)
     end
