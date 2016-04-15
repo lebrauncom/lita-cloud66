60d8841d26bb8d537d0d0a6d67989e69b4915dcd
lebrauncom <lebraun.com@gmail.com>
2016-04-15 14:39:12 +1000

    add routing and simulate response

--- lib/lita/handlers/cloud66.rb
+++ lib/lita/handlers/cloud66.rb
@@ -1,7 +1,14 @@
 module Lita
   module Handlers
     class Cloud66 < Handler
-      # insert handler code here
+      route(/deploy production|redeploy production/i, :deploy, command: false, help: {
+        'deploy production' => 'Deploys the production server.',
+        'redeploy production' => 'Redeploys the production server.'
+      })
+
+      def deploy(response)
+        response.reply "Starting redeployment of production server"
+      end
 
       Lita.register_handler(self)
     end
